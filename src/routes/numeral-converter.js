20d67569782a66350e155d223d0502aea07f3372
MayaraAssis <mayaraassisn@gmail.com>
2024-05-11 12:27:36 -0300

    add vercel

--- src/routes/numeral-converter.js
+++ src/routes/numeral-converter.js
@@ -4,5 +4,9 @@ const Translate = require('../handlers/numeral-converter')
 const routes = Router()
 
 routes.post('/translate', Translate.translateNumber)
+routes.get('/', (req, res) => {
+  console.log('qualuqer ')
+}
 
+)
 module.exports = routes
